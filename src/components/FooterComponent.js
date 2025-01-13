95109c84317b0ff03482dc2ac50a5c2a9482e5c6
wasiq <wroxtaar@gmail.com>
2025-01-13 13:21:24 +0530

    dsd

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <p className='text-center'>Copyrights reserved at 2025</p>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
